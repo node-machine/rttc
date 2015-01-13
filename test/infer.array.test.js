fe25926bb817213af021e5d0e94b43ac0d07b8d1
Mike McNeil <mike@balderdash.co>
2015-01-12 22:58:49 -0800

    Trivial

--- test/infer.array.test.js
+++ test/infer.array.test.js
@@ -1,48 +1,48 @@
 var assert = require('assert');
 var infer = require('../lib/infer');
 
-describe('Inferring types from example', function() {
+describe('Inferring schema from example', function() {
 
   describe('when an array of primitives is used', function() {
 
     it('should parse an array with a single string', function() {
       var arr = ['foo'];
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert.strictEqual(types[0], 'string');
+      assert.strictEqual(schema[0], 'string');
     });
 
     it('should parse an array with a single number', function() {
       var arr = [1];
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert.strictEqual(types[0], 'number');
+      assert.strictEqual(schema[0], 'number');
     });
 
     it('should parse an array with a single `true`', function() {
       var arr = [true];
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert.strictEqual(types[0], 'boolean');
+      assert.strictEqual(schema[0], 'boolean');
     });
 
     it('should parse an array with a single `false`', function() {
       var arr = [false];
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert.strictEqual(types[0], 'boolean');
+      assert.strictEqual(schema[0], 'boolean');
     });
 
   });
@@ -56,17 +56,17 @@ describe('Inferring types from example', function() {
         baz: false
       }];
 
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert(types[0].foo);
-      assert(types[0].bar);
-      assert(types[0].baz);
-      assert.strictEqual(types[0].foo, 'string');
-      assert.strictEqual(types[0].bar, 'number');
-      assert.strictEqual(types[0].baz, 'boolean');
+      assert(schema[0].foo);
+      assert(schema[0].bar);
+      assert(schema[0].baz);
+      assert.strictEqual(schema[0].foo, 'string');
+      assert.strictEqual(schema[0].bar, 'number');
+      assert.strictEqual(schema[0].baz, 'boolean');
     });
 
     it('should parse an array with a nested object', function() {
@@ -80,20 +80,20 @@ describe('Inferring types from example', function() {
         }
       }];
 
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert(types[0].foo);
-      assert(types[0].bar);
-      assert(types[0].bar.foo);
-      assert(types[0].bar.baz);
-      assert(types[0].bar.baz.foo);
+      assert(schema[0].foo);
+      assert(schema[0].bar);
+      assert(schema[0].bar.foo);
+      assert(schema[0].bar.baz);
+      assert(schema[0].bar.baz.foo);
 
-      assert.strictEqual(types[0].foo, 'string');
-      assert.strictEqual(types[0].bar.foo, 'boolean');
-      assert.strictEqual(types[0].bar.baz.foo, 'number');
+      assert.strictEqual(schema[0].foo, 'string');
+      assert.strictEqual(schema[0].bar.foo, 'boolean');
+      assert.strictEqual(schema[0].bar.baz.foo, 'number');
     });
 
     it('should parse an array with many nested objects', function() {
@@ -110,23 +110,23 @@ describe('Inferring types from example', function() {
         }
       }];
 
-      var types = infer(arr);
+      var schema = infer(arr);
 
-      assert(Array.isArray(types));
-      assert.strictEqual(types.length, 1);
+      assert(Array.isArray(schema));
+      assert.strictEqual(schema.length, 1);
 
-      assert(types[0].foo);
-      assert(types[0].bar);
-      assert(types[0].bar.foo);
-      assert(types[0].bar.baz);
-      assert(types[0].bar.baz.foo);
-      assert(types[0].bar.baz.bar);
-      assert(types[0].bar.baz.bar.baz);
+      assert(schema[0].foo);
+      assert(schema[0].bar);
+      assert(schema[0].bar.foo);
+      assert(schema[0].bar.baz);
+      assert(schema[0].bar.baz.foo);
+      assert(schema[0].bar.baz.bar);
+      assert(schema[0].bar.baz.bar.baz);
 
-      assert.strictEqual(types[0].foo, 'string');
-      assert.strictEqual(types[0].bar.foo, 'boolean');
-      assert.strictEqual(types[0].bar.baz.foo, 'number');
-      assert.strictEqual(types[0].bar.baz.bar.baz, 'string');
+      assert.strictEqual(schema[0].foo, 'string');
+      assert.strictEqual(schema[0].bar.foo, 'boolean');
+      assert.strictEqual(schema[0].bar.baz.foo, 'number');
+      assert.strictEqual(schema[0].bar.baz.bar.baz, 'string');
     });
 
   });
