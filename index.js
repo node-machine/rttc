1265e4321b9b1e39f006f2e396061850d8690d9d
Mike McNeil <mike@balderdash.co>
2015-01-12 21:06:18 -0800

    Expose infer(), rttc(), and types for backwards compat.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// Expose `validate()` and `coerce()` methods.
 module.exports = {
+  // Expose `validate()` and `coerce()` methods.
   validate: require('./lib/validate'),
-  coerce: require('./lib/coerce')
+  coerce: require('./lib/coerce'),
+
+  // Support for rttc() and infer()
+  // (these are here for compatibility and will likely
+  //  be removed in a future version)
+  rttc: require('./lib/rttc'),
+  infer: require('./lib/rttc'),
+
+  // Also expose `types` object for compat.
+  types: require('./lib/types')
 };
