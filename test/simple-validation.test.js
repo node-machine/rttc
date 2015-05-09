7dc93a3cda70ee66b236eb4ab0590fd351f1c02d
Mike McNeil <mike@balderdash.co>
2015-05-09 13:53:29 -0500

    Further consolidation and setup of tests.

--- test/simple-validation.test.js
+++ test/simple-validation.test.js
@@ -1,7 +1,128 @@
 var assert = require('assert');
 var validate = require('../lib/validate');
+var coerce = require('../lib/coerce');
 
-describe('Runtime type checking', function() {
+describe('Simple coercion and validation', function() {
+
+  describe('.coerce()', function() {
+
+    describe('to string', function() {
+      it('should coerce undefined to base type', function() {
+        assert.strictEqual(coerce('string', undefined), '');
+      });
+      it('should coerce to base type when it gets: null', function (){
+        assert.strictEqual(coerce('string', null), '');
+      });
+      it('should coerce to base type when it gets: NaN', function (){
+        assert.strictEqual(coerce('string', NaN), '');
+      });
+      it('should coerce to base type when it gets: Infinity', function (){
+        assert.strictEqual(coerce('string', Infinity), '');
+      });
+      it('should coerce to base type when it gets: -Infinity', function (){
+        assert.strictEqual(coerce('string', -Infinity), '');
+      });
+      it('should not touch arbitrary string', function() {
+        assert.strictEqual(coerce('string', 'foo'), 'foo');
+      });
+      it('should not touch empty string', function() {
+        assert.strictEqual(coerce('string', ''), '');
+      });
+      it('should not touch integerish string', function() {
+        assert.strictEqual(coerce('string', '2382'), '2382');
+      });
+      it('should not touch negative integerish string', function() {
+        assert.strictEqual(coerce('string', '-2382'), '-2382');
+      });
+      it('should not touch negative zeroish string', function() {
+        assert.strictEqual(coerce('string', '0'), '0');
+      });
+      it('should not touch decimalish string', function() {
+        assert.strictEqual(coerce('string', '1.325'), '1.325');
+      });
+      it('should not touch negative decimalish string', function() {
+        assert.strictEqual(coerce('string', '-1.325'), '-1.325');
+      });
+      it('should coerce numbers to strings', function() {
+        assert.strictEqual(coerce('string', 2382), '2382');
+        assert.strictEqual(coerce('string', -2382), '-2382');
+        assert.strictEqual(coerce('string', 0), '0');
+        assert.strictEqual(coerce('string', 1.325), '1.325');
+        assert.strictEqual(coerce('string', -1.325), '-1.325');
+      });
+
+    });
+
+    describe('to number', function (){
+      it('should coerce undefined to base type', function() {
+        assert.strictEqual(coerce('number', undefined), 0);
+      });
+      it('should coerce to base type when it gets: null', function (){
+        assert.strictEqual(coerce('number', null), 0);
+      });
+      it('should coerce to base type when it gets: NaN', function (){
+        assert.strictEqual(coerce('number', NaN), 0);
+      });
+      it('should coerce to base type when it gets: Infinity', function (){
+        assert.strictEqual(coerce('number', Infinity), 0);
+      });
+      it('should coerce to base type when it gets: -Infinity', function (){
+        assert.strictEqual(coerce('number', -Infinity), 0);
+      });
+      it('should not touch positive integer', function (){
+        assert.strictEqual(coerce('number', 3), 3);
+      });
+      it('should not touch negative integer', function (){
+        assert.strictEqual(coerce('number', -3), -3);
+      });
+      it('should not touch negative decimal', function (){
+        assert.strictEqual(coerce('number', -3.2), -3.2);
+      });
+      it('should not touch zero', function (){
+        assert.strictEqual(coerce('number', 0), 0);
+      });
+      it('should coerce "3.25" to 3.25', function() {
+        assert.strictEqual(coerce('number', '3.25'), 3.25);
+      });
+      it('should coerce "-3.25" to -3.25', function() {
+        assert.strictEqual(coerce('number', '-3.25'), -3.25);
+      });
+      it('should coerce "0" to 0', function() {
+        assert.strictEqual(coerce('number', '0'), 0);
+      });
+    });
+
+    describe('to boolean', function (){
+      it('should coerce undefined to base type', function() {
+        assert.strictEqual(coerce('boolean', undefined), false);
+      });
+      it('should coerce to base type when it gets: null', function (){
+        assert.strictEqual(coerce('boolean', null), false);
+      });
+      it('should coerce to base type when it gets: NaN', function (){
+        assert.strictEqual(coerce('boolean', NaN), false);
+      });
+      it('should coerce to base type when it gets: Infinity', function (){
+        assert.strictEqual(coerce('boolean', Infinity), false);
+      });
+      it('should coerce to base type when it gets: -Infinity', function (){
+        assert.strictEqual(coerce('boolean', Infinity), false);
+      });
+      it('should not touch true', function() {
+        assert.strictEqual(coerce('boolean', true), true);
+      });
+      it('should not touch false', function() {
+        assert.strictEqual(coerce('boolean', false), false);
+      });
+      it('should coerce "true" to true', function() {
+        assert.strictEqual(coerce('boolean', 'true'), true);
+      });
+      it('should coerce "false" to false', function() {
+        assert.strictEqual(coerce('boolean', 'false'), false);
+      });
+    });
+
+  });
 
   describe('.validate()', function() {
 
