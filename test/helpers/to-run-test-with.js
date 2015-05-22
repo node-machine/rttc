efc3b2558336ae2e26d22fd4cda78e8969d504f2
Mike McNeil <mike@balderdash.co>
2015-05-21 22:44:09 -0500

    Expose isEquivalent helper so that other modules using the rttc spec to run their tests can rely on our comparison implementation instead of rewriting it.

--- test/helpers/to-run-test-with.js
+++ test/helpers/to-run-test-with.js
@@ -6,6 +6,9 @@ var util = require('util');
 var _ = require('lodash');
 var rttc = require('../../');
 var getDisplayType = require('../../lib/helpers/get-display-type');
+var isEquivalent = require('../../spec/helpers/is-equivalent');
+
+
 
 module.exports = function toRunTestWith(transformationFn) {
   return function _runTest(expectations, cb){
@@ -51,36 +54,6 @@ module.exports = function toRunTestWith(transformationFn) {
     // Otherwise compare it against the original value (`actual`)
     var compareTo = expectations.hasOwnProperty('result') ? expectations.result : expectations.actual;
 
-    // Use a lodash equality check, but customize it a bit
-    var _keypath = [];
-    var isEquivalent = _.isEqual(actualResult, compareTo, function(value, other, indexOrKey) {
-
-      // Keep track of indices/keys already traversed in order to dereference the appropriate part
-      // of the type schema (`indexOrKey` will be undefined if this is the top-level)
-      if (!_.isUndefined(indexOrKey)){
-        _keypath.push(indexOrKey);
-      }
-
-      try {
-        // Attempt to look up the appropriate keypath within the type schema, or
-        // use the top-level type schema if we haven't tracked any key/indices traversed
-        // yet.
-        var typeToCompareAgainst = typeSchema;
-        if (_keypath.length > 0) {
-          typeToCompareAgainst = _.get(typeSchema, _keypath.join('.'));
-        }
-        // If this type is a lamda, `.toString()` the functions and compare
-        // them that way.
-        if (typeToCompareAgainst === 'lamda') {
-          return (value.toString() === other.toString());
-        }
-      }
-      catch (e){ return false; }
-
-      // If this is not a lamda input, just let the default lodash isEqual handling
-      // take care of it.
-      return undefined;
-    });
     if (!isEquivalent) {
       return cb(new Error('returned incorrect value: '+getDisplayVal(actualResult)+' (a '+getDisplayType(actualResult)+')'));
     }
