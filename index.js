7edc1393c433c3ac2a64db829f79e0c08ce33da0
Mike McNeil <mike@balderdash.co>
2015-05-24 08:32:42 -0500

    Expose rttc.encode and rttc.decode()

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ module.exports = {
   coerce: require('./lib/coerce'),
   infer: require('./lib/infer'),
   isEqual: require('./lib/is-equal'),
-  getDisplayType: require('./lib/get-display-type')
+  getDisplayType: require('./lib/get-display-type'),
+  encode: require('./lib/encode'),
+  decode: require('./lib/decode')
 };
