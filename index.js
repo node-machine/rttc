dae4d01d6a823cd95628f5b011451088a7fbb48f
Mike McNeil <mike@balderdash.co>
2015-05-24 09:29:47 -0500

    Remove public access to .sanitize().

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = {
   infer: require('./lib/infer'),
   isEqual: require('./lib/is-equal'),
   getDisplayType: require('./lib/get-display-type'),
-  sanitize: require('./lib/sanitize'),
   encode: require('./lib/encode'),
   decode: require('./lib/decode')
 };
