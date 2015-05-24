72fe452d0b37e38919c1b7dc3881aeab12a47136
Mike McNeil <mike@balderdash.co>
2015-05-24 06:51:34 -0500

    Expose is-equal()

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   validate: require('./lib/validate'),
   validateStrict: require('./lib/validate-strict'),
   coerce: require('./lib/coerce'),
-  infer: require('./lib/infer')
+  infer: require('./lib/infer'),
+  isEqual: require('./lib/is-equal')
 };
