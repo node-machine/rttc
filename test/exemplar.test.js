3b370e955412d160dcf5dd095d1ecd2dc6d174d9
Mike McNeil <mike@balderdash.co>
2015-06-25 11:49:31 -0500

    Re-enable the other tests

--- test/exemplar.test.js
+++ test/exemplar.test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var _ = require('lodash');
 var rttc = require('../');
 
-describe.only('.exemplar()', function() {
+describe('.exemplar()', function() {
 
 
   it('should generate correct exemplars', function() {
