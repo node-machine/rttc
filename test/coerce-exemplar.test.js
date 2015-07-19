0e6b2b002fe5327caab775f372473d16e77f0f83
Mike McNeil <mike@balderdash.co>
2015-07-19 03:25:47 -0500

    Set up stub for coerce-exemplar tests.

---
+++ test/coerce-exemplar.test.js
@@ -0,0 +1,14 @@
+var util = require('util');
+var assert = require('assert');
+var _ = require('lodash');
+var rttc = require('../');
+
+describe('.coerceExemplar()', function() {
+
+
+  it.skip('should correctly coerce exemplars', function() {
+    // ...
+  });
+
+
+});
