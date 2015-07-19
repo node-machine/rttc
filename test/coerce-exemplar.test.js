4ca6f0e60140499e5b82fae324fe395dde045129
Mike McNeil <mike@balderdash.co>
2015-07-19 03:46:08 -0500

    Wrote test, and fixed issue w/ single-item arrays.

--- test/coerce-exemplar.test.js
+++ test/coerce-exemplar.test.js
@@ -5,10 +5,39 @@ var rttc = require('../');
 
 describe('.coerceExemplar()', function() {
 
+  it('should coerce all `null` into "*"', function() {
+    verifyDeep(rttc.coerceExemplar(null), '*');
+  });
 
-  it.skip('should correctly coerce exemplars', function() {
-    // ...
+  it('should return "===" if `undefined` is specified at the top-level', function (){
+    assert.equal(rttc.coerceExemplar(undefined), '===');
+  });
+
+  it('should strip nested keys and items w/ undefined values (but keep `null`)', function() {
+    verifyDeep(rttc.coerceExemplar({numDandelions: 1, numAmaranth: 2, numLambsQuarters: undefined, numThistle: 4}), {numDandelions: 1, numAmaranth: 2, numThistle: 4});
+    verifyDeep(rttc.coerceExemplar({numDandelions: [undefined], numAmaranth: 2, numLambsQuarters: undefined, numThistle: 4}), {numDandelions: [], numAmaranth: 2, numThistle: 4});
+    verifyDeep(rttc.coerceExemplar({numDandelions: [null], numAmaranth: 2, numLambsQuarters: null, numThistle: 4}), {numDandelions: ['*'], numAmaranth: 2, numLambsQuarters: '*', numThistle: 4});
   });
 
 
+
+
+
 });
+
+
+
+/**
+ * [verifyDeep description]
+ * @param  {[type]} actual   [description]
+ * @param  {[type]} expected [description]
+ * @return {[type]}          [description]
+ */
+function verifyDeep(actual, expected){
+  assert.deepEqual(actual, expected, util.format('got %s  but should have gotten %s', util.inspect(actual, {depth: null}), util.inspect(expected, {depth: null})) );
+  assert.deepEqual({x: actual}, {x:expected}, util.format('got %s  but should have gotten %s', util.inspect({x: actual}, {depth: null}), util.inspect({x:expected}, {depth: null})) );
+  assert.deepEqual({x: [actual]}, {x:[expected]}, util.format('got %s  but should have gotten %s', util.inspect({x: [actual]}, {depth: null}), util.inspect({x:[expected]}, {depth: null})) );
+  assert.deepEqual([{x: actual}], [{x:expected}], util.format('got %s  but should have gotten %s', util.inspect([{x: actual}], {depth: null}), util.inspect([{x:expected}], {depth: null})) );
+  assert.deepEqual([actual], [expected], util.format('got %s  but should have gotten %s', util.inspect([actual], {depth: null}), util.inspect([expected], {depth: null})) );
+  assert.deepEqual([[actual]], [[expected]], util.format('got %s  but should have gotten %s', util.inspect([[actual]], {depth: null}), util.inspect([[expected]], {depth: null})) );
+}
