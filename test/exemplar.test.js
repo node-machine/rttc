54f9119995586827cdc6c3e14568ae9d2336fcfa
Mike McNeil <mike@balderdash.co>
2015-07-19 03:23:30 -0500

    Added tests for .exemplar() alias.

--- test/exemplar.test.js
+++ test/exemplar.test.js
@@ -3,62 +3,75 @@ var assert = require('assert');
 var _ = require('lodash');
 var rttc = require('../');
 
-describe('.exemplar()', function() {
+describe('.getDefaultExemplar()', function() {
 
 
   it('should generate correct exemplars', function() {
+    CHECK_GENERATES_CORRECT_EXEMPLARS('getDefaultExemplar');
+  });
 
-    // Top-level
-    assert.equal(rttc.exemplar('string'), 'a string');
-    assert.equal(rttc.exemplar('number'), 123);
-    assert.equal(rttc.exemplar('boolean'), true);
-    assert(_.isEqual(rttc.exemplar({}), {}));
-    assert(_.isEqual(rttc.exemplar([]), []));
-    assert.equal(rttc.exemplar('json'), '*');
-    assert.equal(rttc.exemplar('lamda'), '->');
-    assert.equal(rttc.exemplar('ref'), '===');
 
-    // Facted dictionary
-    assert(_.isEqual(rttc.exemplar({x: 'string'}), {x:'a string'}));
-    assert(_.isEqual(rttc.exemplar({x:'number'}), {x:123}));
-    assert(_.isEqual(rttc.exemplar({x:'boolean'}), {x:true}));
-    assert(_.isEqual(rttc.exemplar({x:{}}), {x:{}}));
-    assert(_.isEqual(rttc.exemplar({x:[]}), {x:[]}));
-    assert(_.isEqual(rttc.exemplar({x:'json'}), {x:'*'}));
-    assert(_.isEqual(rttc.exemplar({x:'lamda'}), {x:'->'}));
-    assert(_.isEqual(rttc.exemplar({x:'ref'}), {x:'==='}));
-
-    // Patterned array
-    assert(_.isEqual(rttc.exemplar(['string']), ['a string']));
-    assert(_.isEqual(rttc.exemplar(['number']), [123]));
-    assert(_.isEqual(rttc.exemplar(['boolean']), [true]));
-    assert(_.isEqual(rttc.exemplar([{}]), [{}]));
-    assert(_.isEqual(rttc.exemplar([[]]), [[]]));
-    assert(_.isEqual(rttc.exemplar(['json']), ['*']));
-    assert(_.isEqual(rttc.exemplar(['lamda']), ['->']));
-    assert(_.isEqual(rttc.exemplar(['ref']), ['===']));
-
-    // Patterned array in faceted dictionary
-    assert(_.isEqual(rttc.exemplar({x:['string']}), {x:['a string']}));
-    assert(_.isEqual(rttc.exemplar({x:['number']}), {x:[123]}));
-    assert(_.isEqual(rttc.exemplar({x:['boolean']}), {x:[true]}));
-    assert(_.isEqual(rttc.exemplar({x:[{}]}), {x:[{}]}));
-    assert(_.isEqual(rttc.exemplar({x:[[]]}), {x:[[]]}));
-    assert(_.isEqual(rttc.exemplar({x:['json']}), {x:['*']}));
-    assert(_.isEqual(rttc.exemplar({x:['lamda']}), {x:['->']}));
-    assert(_.isEqual(rttc.exemplar({x:['ref']}), {x:['===']}));
-
-    // Faceted dictionary in patterned array
-    assert(_.isEqual(rttc.exemplar([{x: 'string'}]), [{x:'a string'}]));
-    assert(_.isEqual(rttc.exemplar([{x:'number'}]), [{x:123}]));
-    assert(_.isEqual(rttc.exemplar([{x:'boolean'}]), [{x:true}]));
-    assert(_.isEqual(rttc.exemplar([{x:{}}]), [{x:{}}]));
-    assert(_.isEqual(rttc.exemplar([{x:[]}]), [{x:[]}]));
-    assert(_.isEqual(rttc.exemplar([{x:'json'}]), [{x:'*'}]));
-    assert(_.isEqual(rttc.exemplar([{x:'lamda'}]), [{x:'->'}]));
-    assert(_.isEqual(rttc.exemplar([{x:'ref'}]), [{x:'==='}]));
-
+  it('should generate correct exemplars when using `.exemplar()` alias', function() {
+    CHECK_GENERATES_CORRECT_EXEMPLARS('exemplar');
   });
 
 });
 
+
+
+
+
+// Helpers:
+
+function CHECK_GENERATES_CORRECT_EXEMPLARS (methodName) {
+
+    // Top-level
+    assert.equal(rttc[methodName]('string'), 'a string');
+    assert.equal(rttc[methodName]('number'), 123);
+    assert.equal(rttc[methodName]('boolean'), true);
+    assert(_.isEqual(rttc[methodName]({}), {}));
+    assert(_.isEqual(rttc[methodName]([]), []));
+    assert.equal(rttc[methodName]('json'), '*');
+    assert.equal(rttc[methodName]('lamda'), '->');
+    assert.equal(rttc[methodName]('ref'), '===');
+
+    // Facted dictionary
+    assert(_.isEqual(rttc[methodName]({x: 'string'}), {x:'a string'}));
+    assert(_.isEqual(rttc[methodName]({x:'number'}), {x:123}));
+    assert(_.isEqual(rttc[methodName]({x:'boolean'}), {x:true}));
+    assert(_.isEqual(rttc[methodName]({x:{}}), {x:{}}));
+    assert(_.isEqual(rttc[methodName]({x:[]}), {x:[]}));
+    assert(_.isEqual(rttc[methodName]({x:'json'}), {x:'*'}));
+    assert(_.isEqual(rttc[methodName]({x:'lamda'}), {x:'->'}));
+    assert(_.isEqual(rttc[methodName]({x:'ref'}), {x:'==='}));
+
+    // Patterned array
+    assert(_.isEqual(rttc[methodName](['string']), ['a string']));
+    assert(_.isEqual(rttc[methodName](['number']), [123]));
+    assert(_.isEqual(rttc[methodName](['boolean']), [true]));
+    assert(_.isEqual(rttc[methodName]([{}]), [{}]));
+    assert(_.isEqual(rttc[methodName]([[]]), [[]]));
+    assert(_.isEqual(rttc[methodName](['json']), ['*']));
+    assert(_.isEqual(rttc[methodName](['lamda']), ['->']));
+    assert(_.isEqual(rttc[methodName](['ref']), ['===']));
+
+    // Patterned array in faceted dictionary
+    assert(_.isEqual(rttc[methodName]({x:['string']}), {x:['a string']}));
+    assert(_.isEqual(rttc[methodName]({x:['number']}), {x:[123]}));
+    assert(_.isEqual(rttc[methodName]({x:['boolean']}), {x:[true]}));
+    assert(_.isEqual(rttc[methodName]({x:[{}]}), {x:[{}]}));
+    assert(_.isEqual(rttc[methodName]({x:[[]]}), {x:[[]]}));
+    assert(_.isEqual(rttc[methodName]({x:['json']}), {x:['*']}));
+    assert(_.isEqual(rttc[methodName]({x:['lamda']}), {x:['->']}));
+    assert(_.isEqual(rttc[methodName]({x:['ref']}), {x:['===']}));
+
+    // Faceted dictionary in patterned array
+    assert(_.isEqual(rttc[methodName]([{x: 'string'}]), [{x:'a string'}]));
+    assert(_.isEqual(rttc[methodName]([{x:'number'}]), [{x:123}]));
+    assert(_.isEqual(rttc[methodName]([{x:'boolean'}]), [{x:true}]));
+    assert(_.isEqual(rttc[methodName]([{x:{}}]), [{x:{}}]));
+    assert(_.isEqual(rttc[methodName]([{x:[]}]), [{x:[]}]));
+    assert(_.isEqual(rttc[methodName]([{x:'json'}]), [{x:'*'}]));
+    assert(_.isEqual(rttc[methodName]([{x:'lamda'}]), [{x:'->'}]));
+    assert(_.isEqual(rttc[methodName]([{x:'ref'}]), [{x:'==='}]));
+}
